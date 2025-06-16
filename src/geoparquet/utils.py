0021de8f39256f9ed384be9f04cfed5172f485f1
Espen Klungland <espen.klungland@kartverket.no>
2025-06-16 14:47:29 +0200

    oppretter out mappen automatisk

--- src/geoparquet/utils.py
+++ src/geoparquet/utils.py
@@ -1,6 +1,19 @@
+import os
 from pathlib import Path
 
 
 def get_workdir() -> Path:
     # __file__ is not supported directly in notebooks
     return Path(__file__).parent
+
+
+def create_dir(name: str):
+    try:
+        os.mkdir(name)
+        print(f"Directory '{name}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{name}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{name}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
