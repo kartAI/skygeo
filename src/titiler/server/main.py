2241664797475a740a5988bc889c8d3c31081013
Espen Klungland <espen.klungland@kartverket.no>
2026-02-26 14:27:41 +0100

    titiler demo scaffolding

---
+++ src/titiler/server/main.py
@@ -0,0 +1,20 @@
+"""Minimal COG tiler."""
+
+from titiler.core.factory import TilerFactory
+from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
+
+from fastapi import FastAPI
+
+
+app = FastAPI(title="TiTiler + COG")
+
+cog = TilerFactory()
+app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
+
+add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+
+@app.get("/healthz", description="Health Check", tags=["Health Check"])
+def ping():
+    """Health check."""
+    return {"ding dong": "du har fått kupong!"}
